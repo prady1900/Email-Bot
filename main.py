e29cd6e3cda20ccc529caa3ec7d1b5aa41699ada
Pradnyesh Mali <56378577+prady1900@users.noreply.github.com>
2020-01-05 22:27:02 +0530

    Added Email Validation
    Used Regular Expression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from tkinter import *
+rom tkinter import *
+from tkinter import messagebox
 import smtplib
 from functools import partial
+import re
 
 subject = "Test"
 msg = "This is a test message!"
@@ -9,10 +11,32 @@ EMAIL_ADDRESS = "Your Email Address"
 PASSWORD = "Your Password"
 emailaddr = []
 
+root = Tk()
+
+
+def validate(t):
+    reg = '[\w./-]{1,20}@[\w.-]{1,20}.[A-za-z]{2,3}'
+    if re.search(reg, t):
+        return True
+    else:
+        return False
+
+
+def txt():
+    with open('emails.txt', 'w') as f:
+        for item in emailaddr:
+            f.write("%s\n" % item)
+    f.close()
+
 
 def getmail():
     e = e1.get()
-    emailaddr.append(e)
+    if validate(e)== True:
+        emailaddr.append(e)
+        e1.delete('0', END)
+        txt()
+    else:
+        messagebox.showerror("Error", "Invalid Email")
 
 
 def send_email(subject, msg):
@@ -30,8 +54,7 @@ def send_email(subject, msg):
         print("Message failed")
 
 
-root = Tk()
-root.title("EMAIL BOT")
+root.title("Calculating square root")
 root.geometry('200x50')
 
 Label(root, text="Enter Email: ").grid(column=0, row=0)
@@ -41,6 +64,5 @@ e1.grid(column=1, row=0)
 Button(root, command=getmail, text="Submit").grid(column=0, row=1)
 Button(root, command=partial(send_email, subject, msg), text="Send").grid(column=1, row=1)
 root.mainloop()
-
 
 print(emailaddr)
